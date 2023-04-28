560310c75de69f000417d396b1c3868f6fd60f8f
Mohammad Toosi <m_dd@rogers.com>
2023-04-28 23:45:39 +0430

    feat: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  dir: {
+    assets: "src/assets",
+    layouts: "src/layouts",
+    pages: "src/pages",
+    middleware: "src/middleware",
+  },
+});
---
+++ src/pages/about.vue
@@ -0,0 +1,4 @@
+<template>
+  <div>
+    about 2</div>
+</template>
