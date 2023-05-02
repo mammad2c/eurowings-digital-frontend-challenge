d82523df0c44455d141dd89f9f202701b936c366
Mohammad Toosi <m_dd@rogers.com>
2023-05-02 19:18:02 +0430

    feat: add Quasar and update src directory config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  dir: {
-    assets: "src/assets",
-    layouts: "src/layouts",
-    pages: "src/pages",
-    middleware: "src/middleware",
-  },
+  srcDir: "src",
+  modules: ["nuxt-quasar-ui"],
 });
---
+++ src/ui/button/button.vue
@@ -0,0 +1,13 @@
+<template>
+  <QBtn v-bind="$attrs">
+    <slot />
+  </QBtn>
+</template>
+
+<script setup lang="ts">
+import { QBtn, QBtnProps } from "quasar";
+
+interface ButtonProps extends QBtnProps {}
+
+defineProps<ButtonProps>();
+</script>
