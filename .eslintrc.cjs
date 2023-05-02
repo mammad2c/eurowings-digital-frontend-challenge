fe6a1dbaa0f23d73db5e931b104de625d6ae2b21
Mohammad Toosi <m_dd@rogers.com>
2023-05-02 19:17:33 +0430

    fix: disable Vue 2 eslint errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: ["@nuxtjs/eslint-config-typescript", "prettier"],
+  rules: {
+    "vue/no-multiple-template-root": "off",
+    "vue/multi-word-component-names": "off",
+  },
 };
