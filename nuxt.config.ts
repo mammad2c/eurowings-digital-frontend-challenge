3a70ed3b92fae1fe6fbe62ad00489479fbc01f8b
Mohammad Toosi <m_dd@rogers.com>
2023-05-05 20:57:09 +0430

    refactor: add nuxt test module and downgrade to yarn 1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: "src",
-  modules: ["nuxt-quasar-ui"],
+  modules: ["nuxt-quasar-ui", "nuxt-vitest"],
+  quasar: {
+    extras: {
+      fontIcons: ["material-icons"],
+    },
+  },
 });
