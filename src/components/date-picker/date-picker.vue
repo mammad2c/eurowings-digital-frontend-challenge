4baa6c6042f979e4715236f6997c1b89f122ca94
Mohammad Toosi <m_dd@rogers.com>
2023-05-06 02:50:00 +0430

    chore: add range to date-picker

--- src/components/date-picker/date-picker.vue
+++ src/components/date-picker/date-picker.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="full-width">
     <UIInput
-      v-model="value"
+      v-model="formattedValue"
       :label="label"
       readonly
       class="ui-date-picker-input"
       :rules="rules"
+      :dense="dense"
+      :hide-bottom-space="hideBottomSpace"
     >
       <QPopupProxy cover transition-show="scale" transition-hide="scale">
-        <QDate v-model="value">
+        <QDate v-model="value" :range="range" :options="options">
           <div class="row items-center justify-end">
             <UIButton v-close-popup label="Close" color="primary" flat />
           </div>
@@ -19,13 +21,17 @@
 </template>
 
 <script setup lang="ts">
-import { QDate, QPopupProxy } from "quasar";
+import { QDate, QPopupProxy, QDateProps } from "quasar";
 import { UIButton, UIInputProps, UIInput } from "@/ui";
 
 interface UIDatePickerProps {
-  modelValue: string;
+  modelValue: QDateProps["modelValue"];
   label?: UIInputProps["label"];
   rules?: UIInputProps["rules"];
+  dense?: boolean | undefined;
+  hideBottomSpace?: boolean | undefined;
+  range?: boolean | undefined;
+  options?: QDateProps["options"];
 }
 
 const props = defineProps<UIDatePickerProps>();
@@ -39,13 +45,25 @@ const value = computed({
     emit("update:modelValue", value);
   },
 });
+
+const formattedValue = computed(() => {
+  if (props.range) {
+    if (typeof value.value !== "object") {
+      return value.value;
+    }
+
+    return `${value.value?.from || ""} - ${value.value?.to || ""}`;
+  }
+
+  return value.value;
+});
 </script>
 
 <style lang="scss">
 .ui-date-picker-input {
   &.q-field--readonly {
     .q-field__control::before {
-      border-bottom-style: solid;
+      border-style: solid;
     }
   }
 }
